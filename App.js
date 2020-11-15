4209b09b9e3b7611fc87a34dce8cdc025a873b3b
Diego Cabrera <diego.cabrera.15@outlook.com>
2020-11-15 17:35:25 -0500

    Dimensions screen

--- App.js
+++ App.js
@@ -1,50 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, TextInput, View, Dimensions } from "react-native";
 
-const Texto = ({style}) => {
-  const [texto, setTexto] = useState("Hi World!")
-  const updateText = () => {
-    setTexto("Bye World!")
-  }
-  return (
-    <Text style={[styles.text, style]} onPress={updateText}>{texto}</Text>
-  )
-};
-
+const width = Dimensions.get('window').width
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto style={styles.red} />
-      <Texto style={styles.green} />
-      <Texto style={styles.blue} />    
+      <Text>Text</Text>
+      <TextInput style={styles.input}></TextInput>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  text: {
-    color: 'white',
-    fontSize: 25,
-  },
-  red: {
-    //flex: 1,
-    backgroundColor: 'red',    
-  },
-  green: {
-    //flex: 2,
-    backgroundColor: 'green',
-  },
-  blue: {
-    //flex: 3,
-    backgroundColor: 'blue',
+const styles = StyleSheet.create({  
+  input: {
+    height:40,
+    width: width,//'100%',
+    backgroundColor: '#eee'
   },
   container: {
     flex: 1,
-    flexDirection: 'column',//row,row-reverse, column, column-reverse
     backgroundColor: "#fff",
-    alignItems: "baseline",//center,flex-start, flex-end, stretch, baseline
-    justifyContent: "space-evenly",//center, flex-start, flex-end, space-between, space-around, space-evenly
+    alignItems: "center",//center,flex-start, flex-end, stretch, baseline
+    justifyContent: "center",//center, flex-start, flex-end, space-between, space-around, space-evenly
   },
 });
