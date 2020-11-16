6cda96cadb0b1ba79316b49a62ae4d2d32431d19
Diego Cabrera <diego.cabrera.15@outlook.com>
2020-11-15 22:37:10 -0500

    Uses markers with maps

--- App.js
+++ App.js
@@ -1,27 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Text, Dimensions } from "react-native";
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 import * as Location from "expo-location";
 import Constants from "expo-constants";
 
 
 export default function App() {
+  const [locationCurrent, setLocationCurrent] = useState({})
   const findLocation = async () => {
     const { status } = await Location.requestPermissionsAsync();
     if (status !== "granted") {
       return Alert.alert("We dont have permissions");
     }
-    const location = await Location.getCurrentPositionAsync();
+    const location = await Location.getCurrentPositionAsync({});
     console.log(location);
+    setLocationCurrent(location)
   };
   useEffect(() => {
     findLocation();
   });
   return (
     <View style={styles.container}>
-      <MapView 
-        style={styles.map}
-      />
+      <MapView style={styles.map}>
+        {locationCurrent.coords ? 
+          <Marker
+          coordinate={locationCurrent.coords}
+          title="Title"
+          description="Description point"/>
+          :
+          null
+            
+        }
+      </MapView>
     </View>
   );
 }
