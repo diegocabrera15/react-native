8f98d03215e67f2807dd38df482029db9c3bc94c
Diego Cabrera <diego.cabrera.15@outlook.com>
2020-11-15 21:16:11 -0500

    loading bar or loading spinner with ActivityIndicator

--- App.js
+++ App.js
@@ -1,53 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, FlatList,} from "react-native";
+import React from "react";
+import { StyleSheet, ActivityIndicator, View} from "react-native";
 
 export default function App() {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => {
-        setUsers(data)
-        setLoading(false)        
-      })
-  }, [])
-
-  if(loading){
-    return <View style={styles.center}><Text>Cargando...</Text></View>
-  }
   return (
     <View style={styles.container}>
-      <Text>Cargado!</Text>
-      <FlatList 
-        data={users}
-        renderItem={({item})=> <Text style={styles.item}>{item.name}</Text>}
-        keyExtractor={item => String(item.id)}
+      <ActivityIndicator 
+        size='small' color='#000f'
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  center:{
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "stretch", //center,flex-start, flex-end, stretch, baseline
     justifyContent: "center", //center, flex-start, flex-end, space-between, space-around, space-evenly
     paddingTop: 22,
-  },
-  item: {
-    padding: 10,
-    fontSize: 22,
-    height:50,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1
   },  
 });
