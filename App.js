c1465584441caf5e2a3e2de2c56a3155ab9bec6e
Diego Cabrera <diego.cabrera.15@outlook.com>
2020-11-15 21:52:40 -0500

    Uses of dialogs

--- App.js
+++ App.js
@@ -1,47 +1,31 @@
 import React, {useState} from "react";
-import { StyleSheet, Modal, View, Text, Button, } from "react-native";
+import { StyleSheet, Modal, View, Text, Button, Alert, } from "react-native";
 
+const createDialog = () => Alert.alert(
+  'dialog title', 
+  'sub title or message for dialog',
+  [
+    {
+      text: 'Cancel',
+      onPress: ()=>{},
+      style: "cancel"
+    },
+    {
+      text: 'Accept',
+      onPress: ()=>console.log('boton press'),
+    }
+  ]
+  )
 export default function App() {
   const [modal, setModal] = useState(false)
   return (
     <View style={styles.container}>
-      <Modal 
-        animationType="slide"
-        transparent={true}
-        visible={modal}
-        //onRequestClose permite ejecutar codigo cuando sea haya cerrado el modal
-      >
-        <View style={styles.center}>
-          <View style={styles.content}>
-            <Text>Soy un modal</Text>
-            <Button title='Cerrar modal' onPress={()=>setModal(!modal)}/>
-          </View>
-        </View>
-      </Modal>
-      <Text>No soy un modal</Text>
-      <Text>No soy un modal</Text>
-      <Text>No soy un modal</Text>
-      <Text>No soy un modal</Text>
-      <Text>No soy un modal</Text>
-      <Button title='Abrir modal' onPress={()=>setModal(!modal)}/>
+      <Button title='Open dialog' onPress={createDialog}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  content: {
-    flex:1,
-    backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin:25
-  },
-  center: {
-    flex:1,
-    alignItems:'stretch',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.3)'
-  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
